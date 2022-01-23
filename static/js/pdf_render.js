371e8cd919510c6e995e8c326148c21cfef9aad7
Md. Almas Ali <almaspr3@gmail.com>
2022-01-23 15:30:33 +0600

    Fixed image blur issue.

--- static/js/pdf_render.js
+++ static/js/pdf_render.js
@@ -13,9 +13,10 @@ var pdfDoc = null,
     pageNum = 1,
     pageRendering = false,
     pageNumPending = null,
-    scale = 0.8,
+    scale = 1.5,
     canvas = document.getElementById('the-canvas'),
     ctx = canvas.getContext('2d');
+
 
 /**
  * Get page info from document, resize canvas accordingly, and render page.
